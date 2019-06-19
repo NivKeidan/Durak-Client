961a7a6f781a6cac2aa87508c66a86c370dae314
nivkeidan@gmail.com <nivkeidan@gmail.com>
2019-06-19 20:24:40 +0300

    Refactored menu rendering Added menu for game created but not started

--- src/Menu.js
+++ src/Menu.js
@@ -8,6 +8,9 @@ class Menu extends React.Component {
 
         this.handleSubmit = this.handleSubmit.bind(this);
         this.handlePlayerNumChange = this.handlePlayerNumChange.bind(this);
+        this.renderCreateGameMenu = this.renderCreateGameMenu.bind(this);
+        this.renderJoinGameMenu = this.renderJoinGameMenu.bind(this);
+        this.renderGameInProgressMenu = this.renderGameInProgressMenu.bind(this);
 
         this.state = {
             numOfPlayers: 4
@@ -16,7 +19,7 @@ class Menu extends React.Component {
     }
 
     handleSubmit(e) {
-        this.props.startNewGame(this.state.numOfPlayers)
+        this.props.createNewGame(this.state.numOfPlayers);
         e.preventDefault();
     }
 
@@ -24,24 +27,45 @@ class Menu extends React.Component {
         this.setState({numOfPlayers: parseInt(e.target.value)});
     }
 
+    renderCreateGameMenu() {
+        return (
+            <div className={"menu-options"}>
+                <form onSubmit={this.handleSubmit}>
+                    <label> Number of players:
+                        <select value={this.state.numOfPlayers} onChange={this.handlePlayerNumChange}>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                        </select>
+                    </label>
+                    <input className={"btn-start-game"} type="submit" value="Start Game"/>
+                </form>
+            </div>
+        )
+    }
+
+    renderJoinGameMenu() {
+        return (
+            <div>
+                Add form for joining game
+            </div>
+        )
+    }
+
+    renderGameInProgressMenu() {
+        return (
+            <div> Game running menu </div>
+        )
+    }
+
     render() {
         return (
             <div className={"menu"}>
                 <h2><u>Menu</u></h2>
-                {this.props.isGameRunning ?
-                    null :
-                    <div className={"menu-options"}>
-                        <form onSubmit={this.handleSubmit}>
-                        <label> Number of players:
-                            <select value={this.state.numOfPlayers} onChange={this.handlePlayerNumChange}>
-                                <option value="2">2</option>
-                                <option value="3">3</option>
-                                <option value="4">4</option>
-                            </select>
-                        </label>
-                        <input className={"btn-start-game"} type="submit" value="Start Game" />
-                        </form>
-                    </div>
+                {this.props.isGameCreated ?
+                    this.props.isGameRunning ?
+                        this.renderGameInProgressMenu() : this.renderJoinGameMenu()
+                    : this.renderCreateGameMenu()
                 }
             </div>
         )}
